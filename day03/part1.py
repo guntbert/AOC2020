37a593893f9b4ea0b0fb095a4011e21caf38eb9a
Guntbert Reiter <guntbert@gmx.at>
2020-12-08 22:50:57 +0100

    Add the test data for day03/part1

---
+++ day03/part1.py
@@ -0,0 +1,53 @@
+import argparse
+import os.path
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+INPUT_S = '''\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+'''
+
+def compute(s: str) -> int:
+    for line in s.split():
+        pass
+    # TODO: implement solution here!
+    return 0
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+      (INPUT_S, 7),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
