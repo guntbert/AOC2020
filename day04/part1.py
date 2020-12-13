d3f82a70a41fc224e026f9771b3947d186ace0be
G. Reiter <guntbert@gmx.at>
2020-12-13 20:27:08 +0100

    Starting day04

---
+++ day04/part1.py
@@ -0,0 +1,43 @@
+import argparse
+import os.path
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    for line in s.split():
+        pass
+    # TODO: implement solution here!
+    return 0
+
+INPUT_S = '''\
+Here goes the example input
+'''
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        # put given test cases here
+        (INPUT_S, 42), 
+    ,
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
