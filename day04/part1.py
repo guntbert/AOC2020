c61c1ba94ae14ab8feeeb1e1c7565095cb136108
Guntbert Reiter <guntbert@gmx.at>
2020-12-13 21:25:36 +0100

    Solve day04/part1

--- day04/part1.py
+++ day04/part1.py
@@ -7,22 +7,38 @@ from support import timing
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
+REQUIRED_FIELDS = frozenset(('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'))
 
 def compute(s: str) -> int:
-    for line in s.split():
-        pass
-    # TODO: implement solution here!
-    return 0
+    count_valid = 0
+    for passport in s.split('\n\n'):
+        fields_split = [s.strip().split(':') for s in passport.split()]
+        fieldkeys = set({k: v for k, v in fields_split}.keys())
+        if fieldkeys.issuperset(REQUIRED_FIELDS):
+            count_valid += 1
+    return count_valid
 
 INPUT_S = '''\
-Here goes the example input
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
 '''
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
     (
         # put given test cases here
-        (INPUT_S, 42), 
-    ,
+        (INPUT_S, 2), 
+    )
 )
 def test(input_s: str, expected: int) -> None:
     assert compute(input_s) == expected
